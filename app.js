16f7e9b73036b7cbd15f94901af57b7f2a8db355
Jon E Rankin <jr0436@bris.ac.uk>
2014-04-03 20:13:14 +0100

    can read from db

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/arcadeDB');
+
 var app = express();
 
 app.engine('.html', require('ejs').__express);
@@ -25,26 +30,152 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+var db = mongoose.connection;
+var dbIsOpen = false;
+var YoutubeVids;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  	dbIsOpen = true;
+	var youtubeVidSchema = new mongoose.Schema({
+	  name : String,
+	  url: { type: String }
+	});
+
+
+	var users = new mongoose.Schema({
+	  username : String,
+	  password : String
+	});
+	// Compile a 'Movie' model using the movieSchema as the structure.
+	// Mongoose also creates a MongoDB collection called 'Movies' for these documents.
+	YoutubeVids = mongoose.model('YoutubeVids', youtubeVidSchema);
+	users = mongoose.model('users', users);
+	//lower case?
+
+/*	var usernameAdmin = new users({
+		username : 'admin',
+		password : 'password'
+	})
+
+	usernameAdmin.save(function(err, usernameAdmin) {
+	  if (err) return console.error(err);
+	  console.dir(usernameAdmin);
+	});*/
+/*	//VID1
+	var vid1 = new YoutubeVids({
+		name : vid1,
+		url : '8dqEJSTLOQM'
+	})
+
+	vid1.save(function(err, vid1) {
+	  if (err) return console.error(err);
+	  console.dir(vid1);
+	});*/
+
+/*	YoutubeVids.findOne({ title: 'vid1' }, function(err, vid1) {
+	  if (err) return console.error(err);
+	  console.log(vid1.url);
+	}); */
+
+
+
+	//VID2
+/*	var vid2 = new YoutubeVids({
+		name : vid2,
+		url : 'T4JrQpzno5Y'
+	})
+
+	vid2.save(function(err, vid2) {
+	  if (err) return console.error(err);
+	  console.dir(vid2);
+	});*/
+
+	/*YoutubeVids.findOne({ title: 'vid2' }, function(err, vid2) {
+	  if (err) return console.error(err);
+	  console.log(vid2.url);
+	});*/
+
+
+	//VID3
+/*	var vid3 = new YoutubeVids({
+		name : vid3,
+		url : 'EcKinnMXuKg'
+	})
+
+	vid3.save(function(err, vid3) {
+	  if (err) return console.error(err);
+	  console.dir(vid3);
+	});*/
+
+/*	YoutubeVids.findOne({ title: 'vid3' }, function(err, vid3) {
+	  if (err) return console.error(err);
+	  console.log(vid1.url);
+	});*/
+});
+
+
+
+
+
+
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var findCallback = function(err, data)
+{
+	if (err)
+	{
+		console.log("found error " + err);
+		return;
+	}
+	else
+	{
+		//getScreen(;
+		return data.url;
+	}
+}
 
 
-//app.get('/', routes.index);
 app.get('/', function (req, res){
-	var URL1 = 'http://www.youtube.com/embed/8dqEJSTLOQM';
-	var URL2 = 'http://www.youtube.com/embed/T4JrQpzno5Y';
-	var URL3 = 'http://www.youtube.com/embed/EcKinnMXuKg';
-	res.render('index', {
-		url1 : URL1,
-		url2 : URL2, 
-		url3 : URL3,
-		thumb1 : 'afterlife.png',
-		thumb2 : 'rebellion.png',
-		thumb3 : 'suburbs.png',
-	});
+
+	if(!dbIsOpen){		
+		console.log("db not open" );
+		return;
+	}
+
+	function getScreen( url, size )
+	{
+	  if(url === null){ return ""; }
+
+	  size = (size === null) ? "big" : size;
+	  var vid;
+	  var results;
+
+	  results = url.match("[\\?&]v=([^&#]*)");
+
+	  vid = ( results === null ) ? url : results[1];
+
+	  if(size == "small"){
+	    return "http://img.youtube.com/vi/"+vid+"/2.jpg";
+	  }else {
+	    return "http://img.youtube.com/vi/"+vid+"/0.jpg";
+	  }
+	}
+
+		YoutubeVids.find({}, function(err, videolist){
+			res.render('index', {
+				url1 : videolist[0].url,
+				url2 : videolist[1].url, 
+				url3 : videolist[2].url,
+				thumb1 : getScreen(videolist[0].url),
+				thumb2 : getScreen(videolist[1].url),
+				thumb3 : getScreen(videolist[2].url),
+			});
+		});
 });
 
 app.get('/users', user.list);
